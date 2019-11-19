7511d1509d1faa397eccd3c5413c04417576d1ac
Mustafa ALKAN <mustafaalkan64@gmail.com>
2019-11-19 15:25:51 +0300

    changings on dataservice

--- service/DataService.js
+++ service/DataService.js
@@ -15,52 +15,27 @@ class DataService {
             },
             {
                 "$match": {
-                    "$and": [
-                    ]
+                        "totalCount": {
+                            "$gt": parseInt(params.minCount), "$lt": parseInt(params.maxCount)
+                        }
+                },
+                "$match": {
+                    "createdAt": { 
+                                "$gt": new Date(params.startDate), 
+                                "$lt": new Date(params.endDate) 
+                            }
                 }
             }
         ];
 
         console.dir(params,{depth:null});
-    
-        var dateFilter = {};
-        var countFilter = {};
 
-        // Bu şekilde result dönmedi
         // let countFilter = {};
         // let dateFilter = {};
         // countFilter.totalCount = { "$lt": params.maxCount, "$gt": params.minCount };
         // dateFilter.createdAt = { "$gt": new Date(params.startDate), "$lt": new Date(params.endDate) };
+        // countFilter.totalCount = { "$gt": params.minCount, "$lt": params.maxCount };
 
-        if(params.startDate) {
-            dateFilter.createdAt = { "$gt": new Date(params.startDate) };
-        } 
-    
-        if(params.endDate) {
-            if(dateFilter.createdAt) {
-                dateFilter.createdAt.$lt = new Date(params.endDate);
-            }
-            else {
-                dateFilter.createdAt = { "$lt": new Date(params.endDate) };
-            }
-        }
-    
-        if(params.minCount) {
-            countFilter.totalCount = { "$gt": params.minCount };
-        }
-    
-        if(params.maxCount) {
-            if(countFilter.totalCount) {
-                countFilter.totalCount.$lt = params.maxCount;
-            }
-            else {
-                countFilter.totalCount = { "$lt": params.maxCount };
-            }
-        }
-
-    
-         aggregate[1].$match.$and.push(dateFilter);
-         aggregate[1].$match.$and.push(countFilter);
          return await Data.aggregate(aggregate).exec();
     }
 }
