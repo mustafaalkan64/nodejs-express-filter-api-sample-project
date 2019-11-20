3728cda7b5ecdfd85e359cf6ab9d5991111f358c
Mustafa ALKAN <mustafaalkan64@gmail.com>
2019-11-20 09:44:41 +0300

    index.js modified

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const bodyParser = require('body-parser');
 const api = require('./api');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
-const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://dbUser:dbPassword1@ds249623.mlab.com:49623/getir-case-study";
+const PORT = 8080;
+const DB_CONNECTION_STRING = "mongodb://dbUser:dbPassword1@ds249623.mlab.com:49623/getir-case-study";
 
 mongoose.connect(DB_CONNECTION_STRING, { useNewUrlParser: true });
 const database = mongoose.connection;
