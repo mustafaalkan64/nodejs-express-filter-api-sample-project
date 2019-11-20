395b9e2c09ff607caad3ceb9b375a9437e82ad1e
Mustafa ALKAN <mustafaalkan64@gmail.com>
2019-11-20 09:37:45 +0300

    test file corrected

--- test/filter.test.js
+++ test/filter.test.js
@@ -2,7 +2,7 @@ const DataService = require('../service/DataService');
 
 test('DataService Test Method .', () => {
     const service = new DataService();
-    service.filter({ "startDate": "2016-01-26", "endDate": "2018-02-02" , "minCount": 2800, "maxCount": 3000 })
+    service.filterData({ "startDate": "2016-01-26", "endDate": "2018-02-02" , "minCount": 2800, "maxCount": 3000 })
     .then(result => {
         expect(result.code).toBe(0);
         expect(result.msg).toBe('Success');
